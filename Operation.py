f06c2fa3815c9f16265c74beb93d9b6e01c3a6dc
Nico Wong <nicowong8@gmail.com>
2021-04-30 23:14:54 -0700

    Reordered the functions in class Operations

--- Operation.py
+++ Operation.py
@@ -1,5 +1,14 @@
 
 class Operation:
+
+    @classmethod
+    def Put(cls, key, value):
+        return cls("put", key=key, value=value)
+
+    @classmethod
+    def Get(cls, key):
+        return cls("get", key=key)
+
     def __init__(self, operationType, **kwargs):
         self.type = operationType
         self.key = None
@@ -11,25 +20,6 @@ class Operation:
         if 'value' in kwargs:
             self.value = kwargs['value']
 
-    def __hash__(self):
-        return hash( (self.type, self.key, self.value) )    # Note: If self.value is None for type=="get", it should always be hashed to the same number
-
-    def __eq__(self, other):
-        if self.type == other.type:
-            if self.type == "get":
-                return self.key == other.key
-            elif self.type == "put":
-                return self.key == other.key and self.value == other.value
-        return False
-
-    @classmethod
-    def Put(cls, key, value):
-        return cls("put", key=key, value=value)
-
-    @classmethod
-    def Get(cls, key):
-        return cls("get", key=key)
-
     def __repr__(self):
         rep = f"Operation({repr(self.type)}"
 
@@ -40,4 +30,15 @@ class Operation:
         rep += ")"
 
         return rep
+
+    def __hash__(self):
+        return hash( (self.type, self.key, self.value) )    # Note: If self.value is None for type=="get", it should always be hashed to the same number
+
+    def __eq__(self, other):
+        if self.type == other.type:
+            if self.type == "get":
+                return self.key == other.key
+            elif self.type == "put":
+                return self.key == other.key and self.value == other.value
+        return False
         
